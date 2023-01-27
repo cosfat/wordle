69ceeb73220b2535280f17e576d03c7bfc28a928
cosfat <drcosfat@gmail.com>
2023-01-27 06:52:35 +0300

    USerSummary eklendi

--- resources/views/livewire/user-summary.blade.php
+++ resources/views/livewire/user-summary.blade.php
@@ -1,5 +1,6 @@
 <div class="flex justify-center mt-6">
-    <div class="min-w-screen bg-gray-100 flex items-center justify-center border border-gray-300 bg-gray-100 font-sans overflow-hidden">
+    <div
+        class="min-w-screen bg-gray-100 flex items-center justify-center border border-gray-300 bg-gray-100 font-sans overflow-hidden">
         <div class="w-full lg:w-5/6">
             <div class="bg-white shadow-md rounded my-6">
                 <div class="p-4">
@@ -20,49 +21,53 @@
                     <tbody class="text-gray-600 text-sm font-light">
                     @foreach($games as $game)
                         @if($game->winner_id != null)
-                        <tr class="border-b border-gray-200 hover:bg-gray-100">
-                            <td class="py-3 px-6 text-left whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="mr-2">
+                            <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                        </div>
+                                        <span class="font-medium">{{ $game->user->username }}</span>
                                     </div>
-                                    <span class="font-medium">{{ $game->user->username }}</span>
-                                </div>
-                            </td>
-                            <td class="py-3 px-6 text-left whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="mr-2">
+                                </td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                        </div>
+                                        <span class="font-medium">{{ $game->word->name }}</span>
                                     </div>
-                                    <span class="font-medium">{{ $game->word->name }}</span>
-                                </div>
-                            </td>
-                            <td class="py-3 px-6 text-left whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="mr-2">
+                                </td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                        </div>
+                                        <span class="font-medium">{{ $game->guesses->count() }}</span>
                                     </div>
-                                    <span class="font-medium">{{ $game->guesses->count() }}</span>
-                                </div>
-                            </td>
-                            <td class="py-3 px-6 text-left whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="mr-2">
+                                </td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                        </div>
+                                        @if($game->winner_id == $user->id)
+                                            <span
+                                                class="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">Kazandı</span>
+                                        @else
+                                            <span
+                                                class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Kaybetti</span>
+                                        @endif
                                     </div>
-                                    @if($game->winner_id == $user->id)
-                                        <span
-                                            class="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">Kazandı</span>
-                                    @else
-                                        <span
-                                            class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Kaybetti</span>
-                                    @endif
-                                </div>
-                            </td>
-                            <td class="py-3 px-6 text-left whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="mr-2">
+                                </td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                        </div>
+                                        @if($game->winner_id == $user->id)
+                                            <span class="font-medium">{{ $game->degree }}</span>
+                                        @else
+                                            <span class="font-medium">{{ $game->degree }}</span>
+                                        @endif
                                     </div>
-                                    <span class="font-medium">{{ $game->degree }}</span>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
                         @endif
                     @endforeach
                     </tbody>
