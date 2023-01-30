b048d6986c6078df842bbc1ca6de134dc440a671
cosfat <drcosfat@gmail.com>
2023-01-30 07:44:34 +0300

    Challenge mode

---
+++ app/Http/Livewire/TheChallengeGame.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Challenge;
+use App\Models\Chguess;
+use App\Models\Chuser;
+use App\Models\Game;
+use App\Models\Guess;
+use App\Models\Point;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class TheChallengeGame extends Component
+{
+    public $length;
+    public $gameId;
+    public $word;
+    public $opponents = array();
+
+    public $guessesCount;
+    public $guessesArray;
+
+    protected $listeners = ['chWinner', 'chLoser'];
+
+
+    public function chWinner()
+    {
+        $userId = Auth::id();
+        $game = Challenge::whereId($this->gameId)->first();
+
+        if (Chuser::where('user_id', Auth::id())->where('challenge_id', $this->gameId)->exists()) {
+            if (Challenge::whereId($this->gameId)->where('winner_id', null)->exists()) {
+
+                $game->winner_id = $userId;
+                $game->point = ($game->length - Chguess::whereChallenge_id($this->gameId)->where('user_id', $userId)->count() + 2) * 10;
+                $game->save();
+
+                $point = Point::whereUser_id($userId);
+                if($point->exists()){
+                    $point = $point->first();
+                    $point->point = $point->point + $game->point;
+                    $point->save();
+                }
+                else {
+                    $point = new Point;
+                    $point->user_id = $userId;
+                    $point->point = $game->point;;
+                    $point->save();
+                }
+                return redirect('/finished-challenge-game-watcher/'.$this->gameId);
+            }
+            else{
+                return redirect('/finished-challenge-game-watcher/' . $this->gameId);
+            }
+        }
+        else{
+            session()->flash('message', 'Bu oyunu görme yetkiniz yok.');
+            return redirect()->to('/create-game');
+        }
+
+    }
+
+    public function chLoser()
+    {
+        $game = Game::whereId($this->gameId)->first();
+        $game->winner_id = $game->user_id;
+        $game->degree = (8 - $game->length);
+        $game->save();
+
+        $point = Point::whereUser_id($game->user_id);
+        if($point->exists())
+        {
+            $point = $point->first();
+            $previous = $point->point;
+            $new = $game->degree;
+            $total = $previous + $new;
+            $point->point = $total;
+            $point->save();
+        }
+        else {
+            $point = new Point;
+            $point->user_id = $game->user_id;
+            $point->point = $game->degree;;
+            $point->save();
+
+        }
+        return redirect('/finished-game-watcher/'.$this->gameId);
+    }
+
+
+    public function mount($gameId)
+    {
+        if(Chuser::where('user_id', Auth::id())->where('challenge_id', $gameId)->exists() AND
+            Challenge::whereId($gameId)->where('winner_id', null)->exists())
+        {
+            $game = Challenge::whereId($gameId)->first();
+
+            $guesses = Chguess::where('challenge_id', $game->id)->where('user_id', Auth::id())->get();
+            foreach ($guesses as $guess) {
+                $this->guessesArray[] = $guess->word->name;
+            }
+
+            $this->guessesCount = $guesses->count();
+
+            $this->gameId = $gameId;
+            $this->length = $game->length;
+            foreach ($game->chusers as $chuser) {
+                $this->opponents[] = User::whereId($chuser->user_id)->first()->name;
+            }
+
+        } else {
+            session()->flash('message', 'Bu oyunu görme yetkiniz yok.');
+            return redirect()->to('/create-game');
+        }
+
+    }
+
+    public function render()
+    {
+        $chuser = Chuser::where('challenge_id', $this->gameId)->where('user_id', Auth::id())->first();
+        $chuser->seen = 1;
+        $chuser->save();
+        return view('livewire.the-challenge-game');
+    }
+}
---
+++ resources/views/livewire/the-challenge-game.blade.php
@@ -0,0 +1,439 @@
+<div name="the-challenge-game">
+
+    <div class="flex justify-center mb-4">
+        @foreach($opponents as $opponent)
+        <a href="/user-summary/{{ \App\Models\User::where('username', $opponent)->first()->id }}">
+            <h2 class="text-sm font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500 mr-2">{{ $opponent }}</h2>
+
+            @if(\Illuminate\Support\Facades\Cache::has('user-is-online-' . \App\Models\User::where('username', $opponent)->first()->id))
+                <span class="mt-2 ml-2" style="background-color: chartreuse; height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;">&nbsp;</span>
+            @else
+                <span class="mt-2 ml-2" style="background-color: #494949 ; height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;">&nbsp;</span>
+            @endif
+        </a>
+            @endforeach
+    </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        #game-board {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        .letter-box {
+            border: 4px solid #4f46e5;
+            border-radius: 3px;
+            margin: 2px;
+            font-size: 2rem;
+            font-weight: 700;
+            height: 3rem;
+            width: 3rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-transform: uppercase;
+        }
+
+        .filled-box {
+            border: 4px solid #facc15;
+        }
+
+        .letter-row {
+            display: flex;
+        }
+
+        #keyboard-cont {
+            margin: 1rem 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        #keyboard-cont div {
+            display: flex;
+        }
+
+        .second-row {
+            margin: 0.5rem 0;
+        }
+
+        .keyboard-button {
+            font-size: 1rem;
+            font-weight: 700;
+            padding: 0.5rem;
+            margin: 0 2px;
+            cursor: pointer;
+            text-transform: uppercase;
+        }
+    </style>
+    <div id="game-board">
+    </div>
+    <div id="keyboard-cont">
+        <div class="first-row">
+            <button class="keyboard-button">e</button>
+            <button class="keyboard-button">r</button>
+            <button class="keyboard-button">t</button>
+            <button class="keyboard-button">y</button>
+            <button class="keyboard-button">u</button>
+            <button class="keyboard-button">ı</button>
+            <button class="keyboard-button">o</button>
+            <button class="keyboard-button">p</button>
+            <button class="keyboard-button">ğ</button>
+            <button class="keyboard-button">ü</button>
+        </div>
+        <div class="second-row">
+            <button class="keyboard-button">a</button>
+            <button class="keyboard-button">s</button>
+            <button class="keyboard-button">d</button>
+            <button class="keyboard-button">f</button>
+            <button class="keyboard-button">g</button>
+            <button class="keyboard-button">h</button>
+            <button class="keyboard-button">j</button>
+            <button class="keyboard-button">k</button>
+            <button class="keyboard-button">l</button>
+            <button class="keyboard-button">ş</button>
+            <button class="keyboard-button">i</button>
+        </div>
+        <div class="third-row">
+            <button class="keyboard-button">SİL</button>
+            <button class="keyboard-button">z</button>
+            <button class="keyboard-button">c</button>
+            <button class="keyboard-button">v</button>
+            <button class="keyboard-button">b</button>
+            <button class="keyboard-button">n</button>
+            <button class="keyboard-button">m</button>
+            <button class="keyboard-button">ö</button>
+            <button class="keyboard-button">ç</button>
+            <button class="keyboard-button">Enter</button>
+        </div>
+        <div class="fourth-row mt-2">
+            <button class="keyboard-button bg-red-500 text-white">TEMİZLE</button>
+        </div>
+    </div>
+    <script>
+        let words = JSON.parse({!! json_encode(\App\Models\Word::pluck('name')->toJSON()) !!})
+        let guesses = @json($guessesArray);
+
+        const NUMBER_OF_GUESSES = {{ $length + 1 }};
+        let guessesRemaining = NUMBER_OF_GUESSES;
+        let currentGuess = [];
+        let nextLetter = 0;
+        let rightGuessString = "{{ \App\Models\Challenge::find($gameId)->word->name }}";
+
+        function initBoard() {
+            let board = document.getElementById("game-board");
+
+            for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+                let row = document.createElement("div")
+                row.className = "letter-row"
+
+                for (let j = 0; j < {{ $length }}; j++) {
+                    let box = document.createElement("div")
+                    box.className = "letter-box"
+                    row.appendChild(box)
+                }
+
+                board.appendChild(row)
+            }
+        }
+
+        initBoard()
+
+
+        document.addEventListener("keyup", (e) => {
+
+            if (guessesRemaining === 0) {
+                return
+            }
+
+            let pressedKey = String(e.key)
+            if (pressedKey === "Backspace" && nextLetter !== 0) {
+                deleteLetter()
+                return
+            }
+
+            if (pressedKey === "Enter") {
+                checkGuess()
+                return
+            }
+
+            let found = pressedKey.match(/[a-zöçşığü]/gi)
+            if (!found || found.length > 1) {
+                return
+            } else {
+                insertLetter(pressedKey)
+            }
+        })
+
+
+        let addedGuessesCount = 0;
+        let nextAddedLetter = 0;
+        let addedRow = {{ $length + 1 }};
+        if (guesses !== null) {
+
+            guesses.forEach(function (k) {
+                Array.from(k).forEach(function (m) {
+                    addedLetter = String(m);
+                    insertAddedLetter(addedLetter, k);
+                })
+            })
+        }
+
+
+        function insertAddedLetter(addedLetter, k) {
+
+            let row = document.getElementsByClassName("letter-row")[addedGuessesCount]
+            let box = row.children[nextAddedLetter];
+
+            currentGuess.push(addedLetter)
+            if (nextAddedLetter === {{ $length }} - 1) {
+                checkAddedGuess(addedRow);
+                nextAddedLetter = -1;
+                addedGuessesCount += 1;
+                addedRow -= 1;
+            }
+
+            addedLetter = addedLetter.toLowerCase()
+            box.textContent = addedLetter
+            box.classList.add("filled-box")
+
+            nextAddedLetter += 1
+
+        }
+
+
+        function checkAddedGuess(a) {
+
+
+            let row = document.getElementsByClassName("letter-row")[{{ $length + 1 }} - a]
+            let guessString = ''
+            let rightGuess = Array.from(rightGuessString)
+
+            for (const val of currentGuess) {
+                guessString += val
+            }
+
+            for (let i = 0; i < {{ $length }}; i++) {
+                let letterColor = ''
+                let box = row.children[i]
+                let letter = currentGuess[i]
+
+                let letterPosition = rightGuess.indexOf(currentGuess[i])
+
+                // is letter in the correct guess?
+                if (letterPosition === -1) {
+                    letterColor = '#e3e3e3'
+                } else {
+                    // now, letter is definitely in word
+                    // if letter index and right guess index are the same
+                    // letter is in the right position
+                    if (currentGuess[i] === rightGuess[i]) {
+                        // shade green
+                        letterColor = '#02cc09'
+                    } else {
+                        // shade box yellow
+                        letterColor = 'yellow'
+                    }
+
+                    rightGuess[letterPosition] = "#"
+                }
+
+                box.style.backgroundColor = letterColor;
+                shadeKeyBoard(letter, letterColor)
+            }
+
+
+            if (guessString === rightGuessString) {
+                notifyGame("Tebrikler!")
+                guessesRemaining = 0
+                return
+            } else {
+                guessesRemaining -= 1;
+                currentGuess = [];
+                nextLetter = 0;
+
+                if (guessesRemaining === 0) {
+                    notifyGame(`Kaybettin! Doğru kelime: ${rightGuessString}`)
+                }
+            }
+        }
+
+        function insertLetter(pressedKey) {
+            if (nextLetter === {{ $length }}) {
+                return
+            }
+            pressedKey = pressedKey.toLowerCase()
+
+            let row = document.getElementsByClassName("letter-row")[{{ $length + 1 }} - guessesRemaining]
+            let box = row.children[nextLetter]
+            animateCSS(box, "pulse")
+            box.textContent = pressedKey
+            box.classList.add("filled-box")
+            currentGuess.push(pressedKey)
+            nextLetter += 1
+        }
+
+        function deleteLetter() {
+            let row = document.getElementsByClassName("letter-row")[{{ $length + 1 }} - guessesRemaining]
+            let box = row.children[nextLetter - 1]
+            box.textContent = ""
+            box.classList.remove("filled-box")
+            currentGuess.pop()
+            nextLetter -= 1
+        }
+
+        function checkGuess() {
+            let row = document.getElementsByClassName("letter-row")[{{ $length + 1 }} - guessesRemaining]
+            let guessString = ''
+            let rightGuess = Array.from(rightGuessString)
+
+            for (const val of currentGuess) {
+                guessString += val
+            }
+
+            if (guessString.length != {{ $length }}) {
+                notifyGame("{{ $length }} harfli kelime yazmalısın")
+                return
+            }
+
+            if (!words.includes(guessString)) {
+                notifyGame("Bu kelime veritabanımızda yok")
+                return
+            }
+
+
+            for (let i = 0; i < {{ $length }}; i++) {
+
+                let letterColor = ''
+                let box = row.children[i]
+                let letter = currentGuess[i]
+
+                let letterPosition = rightGuess.indexOf(currentGuess[i])
+                // is letter in the correct guess
+                if (letterPosition === -1) {
+                    letterColor = '#e3e3e3'
+                } else {
+                    // now, letter is definitely in word
+                    // if letter index and right guess index are the same
+                    // letter is in the right position
+                    if (currentGuess[i] === rightGuess[i]) {
+                        // shade green
+                        letterColor = '#02cc09'
+                    } else {
+                        // shade box yellow
+                        letterColor = 'yellow'
+                    }
+
+                    rightGuess[letterPosition] = "#"
+                }
+
+
+                let delay = 250 * i
+                setTimeout(() => {
+                    animateCSS(box, 'flipInX')
+                    //shade box
+                    box.style.backgroundColor = letterColor
+                    shadeKeyBoard(letter, letterColor)
+                }, delay)
+            }
+
+
+            var wordNumber = {{ $length + 1 }} - guessesRemaining;
+
+            Livewire.emit('addChGuess', guessString, {{ $gameId }});
+
+            if (guessString === rightGuessString) {
+                notifyGame("Tebrikler!")
+                guessesRemaining = 0;
+                Livewire.emit('chWinner');
+                return
+            } else {
+                guessesRemaining -= 1;
+                currentGuess = [];
+                nextLetter = 0;
+
+                if (guessesRemaining === 0) {
+                    notifyGame(`Kaybettin! Doğru kelime: ${rightGuessString}`);
+                }
+            }
+        }
+
+        function shadeKeyBoard(letter, color) {
+            for (const elem of document.getElementsByClassName("keyboard-button")) {
+                if (elem.textContent === letter) {
+                    let oldColor = elem.style.backgroundColor
+                    if (oldColor === 'green') {
+                        return
+                    }
+
+                    if (oldColor === 'yellow' && color !== 'green') {
+                        return
+                    }
+
+                    elem.style.backgroundColor = color
+                    break
+                }
+            }
+        }
+
+        document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+            const target = e.target
+
+            if (!target.classList.contains("keyboard-button")) {
+                return
+            }
+            let key = target.textContent
+
+            if (key === "SİL") {
+                key = "Backspace"
+            }
+            if (key === "TEMİZLE") {
+                key = "Backspace";
+                for (x = 0; x < 8; x++) {
+                    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+                }
+            }
+
+            document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+        })
+
+        const animateCSS = (element, animation, prefix = 'animate__') =>
+            // We create a Promise and return it
+            new Promise((resolve, reject) => {
+                const animationName = `${prefix}${animation}`;
+                // const node = document.querySelector(element);
+                const node = element
+                node.style.setProperty('--animate-duration', '0.3s');
+
+                node.classList.add(`${prefix}animated`, animationName);
+
+                // When the animation ends, we clean the classes and resolve the Promise
+                function handleAnimationEnd(event) {
+                    event.stopPropagation();
+                    node.classList.remove(`${prefix}animated`, animationName);
+                    resolve('Animation ended');
+                }
+
+                node.addEventListener('animationend', handleAnimationEnd, {once: true});
+            });
+    </script>
+    @if (session()->has('message'))
+        <script>
+            notifyGame("{{  session('message')  }}")
+        </script>
+    @endif
+    <livewire:guess-recorder></livewire:guess-recorder>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
+            integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+</div>
