28f52b170711cee917d0d3f7e3f2d297d0061034
cosfat <drcosfat@gmail.com>
2023-02-01 11:36:12 +0300

    my games sorting done

--- resources/views/livewire/my-games.blade.php
+++ resources/views/livewire/my-games.blade.php
@@ -101,6 +101,38 @@
         </div>
     </div>
 
+
+    <div class="p-4 gap-2 border-b-2">
+        <div class="flex flex-wrap">
+            <h2 class="text-2xl font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500">
+                Bana sorulanlar</h2>
+        </div>
+        <div class="flex flex-wrap">
+            @if($gamesOpp->count() == 0)
+                <p>Hiç oyunun yok, yeni oyun <a href="/create-game" class="text-indigo-500">başlatmak için tıkla!</a>
+                </p>
+            @endif
+            @foreach($gamesOpp as $game)
+                <a href="/the-game/{{ $game[0]->id }}">
+                    <div
+                        class="p-4 flex flex-col items-center text-center group hover:bg-slate-50 cursor-pointer">
+                        <span class="p-2 rounded-full
+                        bg-red-500 shadow-red-200
+                        text-white shadow-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round"
+                                                                                stroke-linejoin="round"
+                                                                                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/></svg>
+                        </span>
+                        <p class="text-xl font-medium text-slate-700 mt-3">
+                            {{ substr(\App\Models\User::whereId($game[0]->user_id)->first()->username, 0, 9) }}
+                        </p>
+                        <p class="text-sm text-gray-600">{{ $game[0]->created_at->diffForHumans(\Carbon\Carbon::now()) }}</p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
     <div class="p-4 gap-2 border-b-2">
         <div class="flex flex-wrap">
             <h2 class="text-2xl font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500">
@@ -142,38 +174,6 @@
         </div>
     </div>
 
-
-    <div class="p-4 gap-2 border-b-2">
-        <div class="flex flex-wrap">
-            <h2 class="text-2xl font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500">
-                Bana sorulanlar</h2>
-        </div>
-        <div class="flex flex-wrap">
-            @if($gamesOpp->count() == 0)
-                <p>Hiç oyunun yok, yeni oyun <a href="/create-game" class="text-indigo-500">başlatmak için tıkla!</a>
-                </p>
-            @endif
-            @foreach($gamesOpp as $game)
-                            <a href="/the-game/{{ $game[0]->id }}">
-                                <div
-                                    class="p-4 flex flex-col items-center text-center group hover:bg-slate-50 cursor-pointer">
-                        <span class="p-2 rounded-full
-                        bg-red-500 shadow-red-200
-                        text-white shadow-lg">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round"
-                                                                                stroke-linejoin="round"
-                                                                                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/></svg>
-                        </span>
-                                        <p class="text-xl font-medium text-slate-700 mt-3">
-                                            {{ substr(\App\Models\User::whereId($game[0]->user_id)->first()->username, 0, 9) }}
-                                        </p>
-                                    <p class="text-sm text-gray-600">{{ $game[0]->created_at->diffForHumans(\Carbon\Carbon::now()) }}</p>
-                                </div>
-                            </a>
-                    @endforeach
-        </div>
-    </div>
 </div>
 
 @if (session()->has('message'))
