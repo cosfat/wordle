8dcc1521f9bb05e963f8187d168a349d3d10b9cf
cosfat <drcosfat@gmail.com>
2023-02-05 16:21:01 +0300

    chat enter problem solved

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', \App\Http\Livewire\Welcome::class);
-Route::get('/cosfat', \App\Http\Cosfat::class);
 
 Route::get('/status', 'UserController@userOnlineStatus');
 Route::middleware([
