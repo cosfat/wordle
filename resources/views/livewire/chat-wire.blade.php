4f4713e6df4de3870906225f725ffb925381b86a
cosfat <drcosfat@gmail.com>
2023-02-05 07:31:27 +0300

    chat colors

--- resources/views/livewire/chat-wire.blade.php
+++ resources/views/livewire/chat-wire.blade.php
@@ -8,7 +8,7 @@
                     <div class="chat-message">
                         <div class="flex items-end justify-end">
                             <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                                <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-green-200 text-gray-500 ">{{ $message->message }}</span>
+                                <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-green-600 text-white ">{{ $message->message }}</span>
                                 </div>
                             </div>
                         </div>
