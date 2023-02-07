f088810a73554e47167e7ee4582b11f94bea2a0b
cosfat <drcosfat@gmail.com>
2023-02-07 19:26:49 +0300

    BAŞLIKLAR

--- resources/views/livewire/my-games.blade.php
+++ resources/views/livewire/my-games.blade.php
@@ -1,4 +1,4 @@
-<div class="py-4">
+<div class="gap-2 container mx-auto rounded-lg">
     <div class="flex justify-center">
         <h2 class="text-2xl font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500">
             DEVAM EDEN OYUNLAR</h2>
