d9ff28081a6fdaf6b018c47669e0d3fa4e99fe4a
cosfat <drcosfat@gmail.com>
2023-02-08 22:59:26 +0300

    timer

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,28 +32,7 @@ class Kernel extends ConsoleKernel
                 $game->chats()->where('game_type', 1)->delete();
                 $game->delete();
             }
-        })->hourly();
-
-        // 1 gündür tahmini olmayan klasik oyunları sil
-        $schedule->call(function () {
-            $games = Game::where('created_at', '<', Carbon::now()->subDay())
-                ->where('guesscount', 0)->get();
-            foreach ($games as $game) {
-                $game->chats()->where('game_type', 1)->delete();
-                $game->delete();
-            }
-        })->hourly();
-
-        // 1 gündür tahmini olmayan Rekabet oyunlarını sil
-        $schedule->call(function () {
-            $challenges = Challenge::where('guesscount', 0)->where('created_at', '<', Carbon::now()->subDay())->get();
-            foreach ($challenges as $challenge) {
-                $challenge->chats()->where('game_type', 2)->delete();
-                $challenge->chusers()->delete();
-                $challenge->delete();
-            }
-        })->hourly();
-
+        })->everyMinute();
 
         // 1 haftadır galibi olmayan Rekabet oyunlarını sil
         $schedule->call(function () {
@@ -63,8 +42,27 @@ class Kernel extends ConsoleKernel
                 $challenge->chusers()->delete();
                 $challenge->delete();
             }
-        })->hourly();
+        })->everyMinute();
 
+        // 1 gündür tahmini olmayan klasik oyunları sil
+        $schedule->call(function () {
+            $games = Game::where('created_at', '<', Carbon::now()->subDay())
+                ->where('guesscount', 0)->get();
+            foreach ($games as $game) {
+                $game->chats()->where('game_type', 1)->delete();
+                $game->delete();
+            }
+        })->everyMinute();
+
+        // 1 gündür tahmini olmayan Rekabet oyunlarını sil
+        $schedule->call(function () {
+            $challenges = Challenge::where('guesscount', 0)->where('created_at', '<', Carbon::now()->subDay())->get();
+            foreach ($challenges as $challenge) {
+                $challenge->chats()->where('game_type', 2)->delete();
+                $challenge->chusers()->delete();
+                $challenge->delete();
+            }
+        })->everyMinute();
     }
 
     /**
