b32268079a42127935a193cfc0c20efbfef085de
cosfat <drcosfat@gmail.com>
2023-02-11 13:16:58 +0300

    Scoreboard design

--- resources/views/livewire/leader-board.blade.php
+++ resources/views/livewire/leader-board.blade.php
@@ -1,59 +1,24 @@
-<div class="p-5 border-b-2 container mx-auto">
+<div class="mt-2">
+    <div class="flex justify-center">
+        <h2 class="text-2xl font-bold tracking-tight sm:text-center sm:text-4xl text-indigo-500">
+            SKOR TABLOSU</h2>
+    </div>
+    <div class="flex justify-center">
+        <span class="text-sm">Her pazartesi sıfırlanır</span>
+    </div>
     @include('loading')
-    <div class="p-6 rounded-lg shadow-md p-4 dark:bg-gray-900 dark:border-gray-600 dark:text-white">
-        <div class="flex justify-between items-center mb-4">
-            <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Skor tablosu (her hafta yenilenir)</h3>
-        </div>
-
+    <div class="p-6 rounded-lg shadow-md p-4">
         <div class="flex justify-between items-center mb-2">
-            <h3 class="text-xl leading-none text-gray-900 dark:text-white">Arkadaşlarım</h3>
+            <h3 class="text-xl leading-none text-gray-900 ">Arkadaşlarım</h3>
         </div>
         <div class="flow-root">
-            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <ul role="list" class="divide-y divide-gray-200">
                 @foreach($friends as $friend)
-                    <a href="/user-summary/{{ $friend->user->id }}">
-                    <li class="py-3 sm:py-2">
-                        <div class="flex items-center space-x-4">
-
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                @if(\Illuminate\Support\Facades\Cache::has('user-is-online-' . $friend->user->id))
-                                    <span style="background-color: chartreuse; height: 25px;
-  width: 25px;
-  border-radius: 50%;
-  display: inline-block;">&nbsp;</span>
-                                @else
-                                    <span style="background-color: #494949 ; height: 25px;
-  width: 25px;
-  border-radius: 50%;
-  display: inline-block;">&nbsp;</span>
-                                @endif
-                            </div>
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    {{ $friend->user->name }}
-                                </p>
-                            </div>
-                            <div
-                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $friend->user->point->point}}
-                            </div>
-                        </div>
-                    </li>
-                    </a>
-                @endforeach
-            </ul>
-        </div>
-        <div class="flex justify-between items-center mb-2 mt-4">
-            <h3 class="text-xl leading-none text-gray-900 dark:text-white">Herkes</h3>
-        </div>
-        <div class="flow-root">
-            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-                @foreach($all as $friend)
                     <a href="/user-summary/{{ $friend->user->id }}">
                         <li class="py-3 sm:py-2">
                             <div class="flex items-center space-x-4">
 
-                                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                <div class="inline-flex items-center text-base font-semibold text-gray-900 ">
                                     @if(\Illuminate\Support\Facades\Cache::has('user-is-online-' . $friend->user->id))
                                         <span style="background-color: chartreuse; height: 25px;
   width: 25px;
@@ -67,12 +32,50 @@
                                     @endif
                                 </div>
                                 <div class="flex-1 min-w-0">
-                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                    <p class="text-sm font-medium text-gray-900 truncate ">
                                         {{ $friend->user->name }}
                                     </p>
                                 </div>
                                 <div
-                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    class="inline-flex items-center text-base font-semibold text-gray-900 ">
+                                    {{ $friend->user->point->point}}
+                                </div>
+                            </div>
+                        </li>
+                    </a>
+                @endforeach
+            </ul>
+        </div>
+        <div class="flex justify-between items-center mb-2 mt-4">
+            <h3 class="text-xl leading-none text-gray-900 ">Herkes</h3>
+        </div>
+        <div class="flow-root">
+            <ul role="list" class="divide-y divide-gray-200">
+                @foreach($all as $friend)
+                    <a href="/user-summary/{{ $friend->user->id }}">
+                        <li class="py-3 sm:py-2">
+                            <div class="flex items-center space-x-4">
+
+                                <div class="inline-flex items-center text-base font-semibold text-gray-900 ">
+                                    @if(\Illuminate\Support\Facades\Cache::has('user-is-online-' . $friend->user->id))
+                                        <span style="background-color: chartreuse; height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;">&nbsp;</span>
+                                    @else
+                                        <span style="background-color: #494949 ; height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;">&nbsp;</span>
+                                    @endif
+                                </div>
+                                <div class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate ">
+                                        {{ $friend->user->name }}
+                                    </p>
+                                </div>
+                                <div
+                                    class="inline-flex items-center text-base font-semibold text-gray-900 ">
                                     {{ $friend->user->point->point}}
                                 </div>
                             </div>
