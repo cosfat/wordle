ba99e3a03f53e9651a4f94fe614460f2bdb116ca
cosfat <drcosfat@gmail.com>
2023-02-18 19:14:42 +0300

    colors

--- resources/views/livewire/contact-wire.blade.php
+++ resources/views/livewire/contact-wire.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="mt-2">
     <div class="cursor-pointer flex justify-center px-5 py-3 font-medium text-slate-700 shadow-xl @if($isContact == 1) bg-yellow-400 @else bg-white @endif "wire:click="state">
         {{ $message }}
     </div>
