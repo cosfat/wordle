b91d26f7509bfbd98ffb3c74e59cf79edb922d58
cosfat <drcosfat@gmail.com>
2023-02-21 14:27:03 +0300

    back buttton small

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,7 +139,7 @@
                     @else
                         <a href="{{ url()->previous() }}">
                     @endif
-                            <svg width="64px" height="64px" viewBox="0 0 1024 1024" fill="#4F46E5FF" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#4F46E5FF" stroke-width="77.824"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill=""></path></g></svg>
+                            <svg width="40px" height="40px" viewBox="0 0 1024 1024" fill="#4F46E5FF" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#4F46E5FF" stroke-width="77.824"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill=""></path></g></svg>
                     </a>
                 </div>
 
