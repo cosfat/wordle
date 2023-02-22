41e424c0112b3e5ab8a40ac65335cd35c5e8f379
cosfat <drcosfat@gmail.com>
2023-02-23 01:12:26 +0300

    today winner loser count

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function userOnlineStatus()
     {
-        $users = User::all();
+        $users = User::orderBy('last_seen', 'desc')->get();
         foreach ($users as $user) {
             if (Cache::has('user-is-online-' . $user->id))
                 echo $user->name . " is online. Last seen: " . Carbon::parse($user->last_seen)->diffForHumans() . " <br>";
--- app/Http/Livewire/MyGames.php
+++ app/Http/Livewire/MyGames.php
@@ -70,16 +70,16 @@ class MyGames extends Component
         }
         $todayId = $today->id;
 
-        $todayRequest = Game::where('today_id', $todayId)->where('winner_id', '!=', 2)->where('winner_id', '!=', null);
+        $todayRequest = Game::where('today_id', $todayId)->where('winner_id', '!=', null);
         $this->todayWinners = $todayRequest->where('winner_id', '!=', 2)->count();
-        $this->todayLosers = $todayRequest->where('winner_id', 2)->count();
-        $fastest = $todayRequest->orderBy('duration', 'asc')->first();
+        $this->todayLosers = Game::where('today_id', $todayId)->where('winner_id', 2)->count();
+        $fastest = Game::where('today_id', $todayId)->where('winner_id', '!=', null)->orderBy('duration', 'asc')->first();
         if($fastest != null){
             $this->fastName = User::find($fastest->opponent_id)->username;
             $this->fastId = $fastest->id;
             $this->fastValue = $this->secondHuman($fastest->duration);
         }
-        $shortest = $todayRequest->orderBy('guesscount', 'asc')->orderBy('duration', 'asc')->first();
+        $shortest = Game::where('today_id', $todayId)->where('winner_id', '!=', null)->orderBy('guesscount', 'asc')->orderBy('duration', 'asc')->first();
         if($shortest != null){
             $this->shortName = User::find($shortest->opponent_id)->username;
             $this->shortId = $shortest->id;
