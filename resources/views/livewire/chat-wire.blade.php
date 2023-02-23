d0b8ea8c01d3082c34c80ed2c2946de2c9a2fd96
cosfat <drcosfat@gmail.com>
2023-02-23 20:06:56 +0300

    times at chat

--- resources/views/livewire/chat-wire.blade.php
+++ resources/views/livewire/chat-wire.blade.php
@@ -21,7 +21,7 @@
                         <div class="flex items-end">
                             <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
                                 <div class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-indigo-500">
-                                    <span class="text-white" style="font-size: 8px">{{ \Carbon\Carbon::parse($message->created_at)->isoFormat('Do MMMM, h:mm') }}</span>
+                                    <span class="text-white" style="font-size: 8px">{{ \Carbon\Carbon::parse($message->created_at)->isoFormat('Do MMMM, H:mm') }}</span>
                                     <span class="text-white text-sm"><strong>{{ \App\Models\User::find($message->user_id)->username }}:</strong> {{ $message->message }}</span>
                                 </div>
                             </div>
