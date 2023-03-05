3a279070ac92fc5af5308aa69e18ffda934a91ea
cosfat <drcosfat@gmail.com>
2023-03-05 05:24:08 +0300

    frequent first starts

--- resources/views/livewire/contact-wire.blade.php
+++ resources/views/livewire/contact-wire.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-4 flex justify-center">
-    <div class="cursor-pointer flex justify-center px-5 py-3 font-medium text-slate-700 shadow-xl @if($isContact == 1) bg-red-500 @else bg-yellow-400 @endif "wire:click="state">
+    <div class="cursor-pointer flex justify-center px-5 py-3 font-medium text-slate-700 shadow-xl @if($isContact == 1) text-white bg-red-500 @else bg-yellow-400 @endif "wire:click="state">
         {{ $message }}
     </div>
 </div>
